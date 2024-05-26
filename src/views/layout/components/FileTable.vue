bade9bcf92d911de6197b91da79f54e495824a15
AAAkater <1318382761@qq.com>
2024-05-26 15:11:11 +0800

    feat:add some options and examples

--- src/views/layout/components/FileTable.vue
+++ src/views/layout/components/FileTable.vue
@@ -1,26 +1,38 @@
 <script setup lang="ts">
-import { ElIcon, ElTable, ElTableColumn } from "element-plus"
+import { ElSpace, ElIcon, ElMessage, ElTable, ElTableColumn, ElPopover, ElInput } from "element-plus"
 import { ref, type Ref } from "vue";
 
 import {
     Document,
-    Menu as IconMenu,
-    Share,
     VideoPlay,
     MoreFilled,
+    Download,
+    DocumentDelete,
+    Edit,
+    Picture,
+    Headset,
 } from "@element-plus/icons-vue";
 
-interface file{
+
+interface option {
+    name: string
+    icon: any
+    click: Function
+}
+
+interface file {
     name: string
     isFolder: boolean
     updateTime: string
     id: string
     fileType: string
     size: number
-    icon:any
+    icon: any
+    isVisible: boolean
+    options: option[]
 }
 
-const files:Ref<file[]> = ref([
+const files: Ref<file[]> = ref([
     {
         name: "123",
         isFolder: false,
@@ -28,7 +40,14 @@ const files:Ref<file[]> = ref([
         id: "adsacafasf",
         fileType: "image",
         size: 22,
-        icon:Document
+        icon: Picture,
+        isVisible: false,
+        options: [
+            { name: "下载", icon: Download, click: () => { ElMessage('下载') } },
+            { name: "删除", icon: DocumentDelete, click: () => { ElMessage('删除') } },
+            { name: "编辑", icon: Edit, click: () => { ElMessage('编辑') } },
+            { name: "更多", icon: MoreFilled, click: () => { ElMessage('更多') } },
+        ]
     },
     {
         name: "sffdasd",
@@ -37,38 +56,93 @@ const files:Ref<file[]> = ref([
         id: "adsacaf",
         fileType: "video",
         size: 22,
-        icon:VideoPlay
+        icon: VideoPlay,
+        isVisible: false,
+        options: [
+            { name: "下载", icon: Download, click: () => { ElMessage('下载') } },
+            { name: "删除", icon: DocumentDelete, click: () => { ElMessage('删除') } },
+            { name: "编辑", icon: Edit, click: () => { ElMessage('编辑') } },
+            { name: "更多", icon: MoreFilled, click: () => { ElMessage('更多') } },
+        ]
+    },
+    {
+        name: "sgdfhnd",
+        isFolder: false,
+        updateTime: "2013-01-12",
+        id: "bsdfsdfg",
+        fileType: "document",
+        size: 22,
+        icon: Document,
+        isVisible: false,
+        options: [
+            { name: "下载", icon: Download, click: () => { ElMessage('下载') } },
+            { name: "删除", icon: DocumentDelete, click: () => { ElMessage('删除') } },
+            { name: "编辑", icon: Edit, click: () => { ElMessage('编辑') } },
+            { name: "更多", icon: MoreFilled, click: () => { ElMessage('更多') } },
+        ]
+    },
+    {
+        name: "basdf",
+        isFolder: false,
+        updateTime: "2024-01-12",
+        id: "bsddfhdhfdfg",
+        fileType: "audio",
+        size: 22,
+        icon: Headset,
+        isVisible: false,
+        options: [
+            { name: "下载", icon: Download, click: () => { ElMessage('下载') } },
+            { name: "删除", icon: DocumentDelete, click: () => { ElMessage('删除') } },
+            { name: "编辑", icon: Edit, click: () => { ElMessage('编辑') } },
+            { name: "更多", icon: MoreFilled, click: () => { ElMessage('更多') } },
+        ]
     }
 ])
+
+const mouseEnter = (row: file) => {
+    row.isVisible = true
+}
+
+const mouseLeave = (row: file) => {
+    row.isVisible = false
+}
+
 </script>
 <template>
-    <el-table :data="files" class="table" :default-sort="{ prop: 'date', order: 'descending'}">
-        <el-table-column type="selection" width="55"/>
-        <el-table-column label="文件名" width="180" >
+    <el-table :data="files" class="table" :default-sort="{ prop: 'date', order: 'descending' }"
+        @cell-mouse-enter="mouseEnter" @cell-mouse-leave="mouseLeave">
+        <el-table-column type="selection" width="55" />
+        <el-table-column label="文件名" width="180">
             <template #default="scope">
                 <div style="display: flex; align-items: center">
-                <el-icon>
-                    <component :is="scope.row.icon"></component>
-                </el-icon>
-                <span style="margin-left: 10px">{{ scope.row.name }}</span>
+                    <el-icon>
+                        <component :is="scope.row.icon"></component>
+                    </el-icon>
+                    <span style="margin-left: 10px">{{ scope.row.name }}</span>
                 </div>
             </template>
         </el-table-column>
-        <el-table-column  label="">
-        <template #default="scope">
-            <div style="display: flex; align-items: center" v-show="scope.row.isHovered">
- <el-icon><Download /></el-icon>
-<el-icon><DocumentDelete /></el-icon>
-<el-icon><Share /></el-icon>
-<el-icon><Edit /></el-icon>
-<el-icon><MoreFilled /></el-icon>
-            </div>
-        </template>
+        <el-table-column width="180">
+            <template #default="scope">
+                <div style="display: flex; align-items: center;justify-content: space-around;"
+                    v-if="scope.row.isVisible">
+                    <el-popover effect="light" trigger="hover" placement="top" width="auto"
+                        v-for="option in scope.row.options">
+                        <template #default>
+                            <h5 style="text-align: center;">{{ option.name }}</h5>
+                        </template>
+                        <template #reference>
+                            <el-icon>
+                                <component :is="option.icon" />
+                            </el-icon>
+                        </template>
+                    </el-popover>
+                </div>
+            </template>
         </el-table-column>
         <el-table-column prop="updateTime" label="修改日期" width="180" />
         <el-table-column prop="fileType" label="文件类型" width="180" />
         <el-table-column prop="size" label="大小" />
-
     </el-table>
 </template>
 
