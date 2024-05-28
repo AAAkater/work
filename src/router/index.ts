01595d2c48c217571082dea468fd5e45e77a5770
AAAkater <1318382761@qq.com>
2024-05-28 21:01:54 +0800

    fix:dashboard router

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,8 +4,8 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      component: import("@/views/layout/layoutContainer.vue"),
+      path: "/dashboard",
+      component: () => import("@/views/dashboard/layout.vue"),
     },
     // {
     //   path: '/about',
